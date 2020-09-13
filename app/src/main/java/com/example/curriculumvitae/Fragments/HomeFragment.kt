c5aa8f8365f1f6ea811943559cd463ce5c8a46e8
medhatmebed <medhat.mebed@gmail.com>
2020-09-13 15:18:42 -0500

    finish setup

--- app/src/main/java/com/example/curriculumvitae/Fragments/HomeFragment.kt
+++ app/src/main/java/com/example/curriculumvitae/Fragments/HomeFragment.kt
@@ -1,18 +1,27 @@
 package com.example.curriculumvitae.Fragments
 
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
 
 import  com.example.curriculumvitae.R
+import kotlinx.android.synthetic.main.fragment_home.*
 
 /**
  * A simple [Fragment] subclass.
  */
 class HomeFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,6 +29,24 @@ class HomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        roundImage()
+    }
+
+
+
+
+
+    private fun roundImage() {
+        val img = BitmapFactory.decodeResource(resources,R.drawable.img_5116)
+        val round = RoundedBitmapDrawableFactory.create(resources,img)
+        round.isCircular = true
+        imageView.setImageDrawable(round)
     }
 
 
